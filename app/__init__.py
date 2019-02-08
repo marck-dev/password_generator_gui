49a1ceee8a5c2266535f8f5935be241bc9b62946
Marck C. Guzmán <marck.guzman.dev@gmail.com>
2019-02-08 23:42:00 +0100

    Create the app functions
    Signed-off-by: Marck C. Guzmán <marck.guzman.dev@gmail.com>

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+import eel as app
+from core import generator as gen
+
+app.init("view")
+
+# func for gen the pass and return
+@app.expose
+def ev(leng):
+    app.setPass(get_pass(leng))
+def get_pass( length ):
+    pas_gen = gen( length )
+    pas_gen.generate() #generate the pass
+    password = pas_gen.get_password() # get the pass
+    del(pas_gen)
+    return password
+
+
+# init the app
+app.start("index.html")
